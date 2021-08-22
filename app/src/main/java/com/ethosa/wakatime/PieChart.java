f10907a815eea15b494da72822cc9fb36315fa70
Ethosa <social.ethosa@gmail.com>
2021-08-23 06:01:49 +0700

    update PieChart.java :eyes:

--- app/src/main/java/com/ethosa/wakatime/PieChart.java
+++ app/src/main/java/com/ethosa/wakatime/PieChart.java
@@ -9,10 +9,13 @@ import android.view.View;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 
 public class PieChart extends View {
     private final Paint paint = new Paint();
+    private HashMap<String, Number> data;
+    private float space = 1f;
 
     public PieChart(Context context) {
         super(context);
@@ -34,6 +37,12 @@ public class PieChart extends View {
         paint.setStyle(Paint.Style.FILL);
         paint.setAntiAlias(true);
         paint.setColor(Color.rgb(255, 77, 255));
+
+        data = new HashMap<>();
+    }
+
+    public float getSpace() {
+        return space;
     }
 
     @Override
@@ -41,24 +50,37 @@ public class PieChart extends View {
         super.onDraw(canvas);
         final float ratio = (getWidth()/getHeight())*(getWidth()+getHeight())/2;
         float max_value = 0f;
+        float max_angle = 360f;
         float current_angle = 0f;
-
-        final HashMap<String, Number> data = new HashMap<>();
-        data.put("Py", 3f);
-        data.put("Rust", 10f);
-        data.put("Java", 11f);
 
         for(Map.Entry<String, Number> entry : data.entrySet()) {
             max_value += (float)entry.getValue();
+            max_angle -= space;
         }
 
         for(Map.Entry<String, Number> entry : data.entrySet()) {
-            final float angle = 360*((float)entry.getValue() / max_value);
-            if (current_angle > 180f)
-                paint.setColor(Color.rgb(100, 222, 100));
-            canvas.drawArc(0f, 0f, ratio, ratio,
+            final float angle = max_angle*((float)entry.getValue() / max_value);
+            generateColor();
+            canvas.drawArc(0f, 0f, ratio, ratio,   // View box
                            current_angle, angle, true, paint);
-            current_angle += angle;
+            current_angle += angle + space;
         }
+    }
+
+    public void putData(String key, float value) {
+        data.put(key, value);
+    }
+
+    public void setSpace(float s) {
+        space = s;
+    }
+
+    private void generateColor() {
+        final Random r = new Random();
+        paint.setColor(Color.rgb(
+                r.nextInt((255 - 144) + 1) + 144,
+                r.nextInt((255 - 144) + 1) + 144,
+                r.nextInt((255 - 144) + 1) + 144
+        ));
     }
 }
