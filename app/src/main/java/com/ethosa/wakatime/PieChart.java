3947af418ec7ff2a6a66677da82ef4226e4dea97
Ethosa <social.ethosa@gmail.com>
2021-08-22 22:04:05 +0700

    added primitive drawing :japanese_goblin:

--- app/src/main/java/com/ethosa/wakatime/PieChart.java
+++ app/src/main/java/com/ethosa/wakatime/PieChart.java
@@ -1,40 +1,44 @@
 package com.ethosa.wakatime;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.text.TextPaint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
-/**
- * TODO: document your custom view class.
- */
+
 public class PieChart extends View {
+    private final Paint paint = new Paint();
+    private RectF rectf;
     public PieChart(Context context) {
         super(context);
-        init(null, 0);
+        init();
     }
 
     public PieChart(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(attrs, 0);
+        init();
     }
 
     public PieChart(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        init(attrs, defStyle);
+        init();
     }
 
-    private void init(AttributeSet attrs, int defStyle) {
+    private void init() {
+        paint.setStrokeWidth(1f);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setAntiAlias(true);
+        paint.setColor(Color.rgb(255, 77, 255));
     }
-
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        final float ratio = (getWidth()/getHeight())*(getWidth()+getHeight())/2;
+        canvas.drawArc(0f, 0f, ratio, ratio, 0f, 360f, true, paint);
     }
 }
