ca708093b6b97cb79a303c2b97383857295c8dcc
Ethosa <social.ethosa@gmail.com>
2021-08-22 22:34:57 +0700

    update PieChart.java :eyes:

--- app/src/main/java/com/ethosa/wakatime/PieChart.java
+++ app/src/main/java/com/ethosa/wakatime/PieChart.java
@@ -4,14 +4,16 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class PieChart extends View {
     private final Paint paint = new Paint();
-    private RectF rectf;
+
     public PieChart(Context context) {
         super(context);
         init();
@@ -37,8 +39,24 @@ public class PieChart extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         final float ratio = (getWidth()/getHeight())*(getWidth()+getHeight())/2;
-        canvas.drawArc(0f, 0f, ratio, ratio, 0f, 360f, true, paint);
+        float max_value = 0f;
+        float current_angle = 0f;
+
+        final HashMap<String, Number> data = new HashMap<>();
+        data.put("Py", 3f);
+        data.put("Rust", 10f);
+        data.put("Java", 11f);
+
+        for(Map.Entry<String, Number> entry : data.entrySet()) {
+            max_value += (float)entry.getValue();
+        }
+
+        for(Map.Entry<String, Number> entry : data.entrySet()) {
+            final float angle = current_angle + (360f * (float)entry.getValue() / max_value);
+            canvas.drawArc(0f, 0f, ratio, ratio,
+                           current_angle, angle, false, paint);
+            current_angle += angle + 10f;
+        }
     }
 }
