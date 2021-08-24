8287c81f7ba43a8e3f4e8f69428d54332ab800b6
Ethosa <social.ethosa@gmail.com>
2021-08-24 14:34:21 +0700

    add Wakatime API usage :eyes:

--- app/src/main/java/com/ethosa/wakatime/MainActivity.java
+++ app/src/main/java/com/ethosa/wakatime/MainActivity.java
@@ -3,15 +3,21 @@ package com.ethosa.wakatime;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.ethosa.wakatime.databinding.ActivityMainBinding;
 
+import com.ethosa.wakatime.models.WakatimeStats;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private SharedPreferences preferences;
     private String apiKey = "";
+    private WakatimeAPI api;
+    private WakatimeStats stats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +25,41 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         preferences = getPreferences(MODE_PRIVATE);
+        api = new WakatimeAPI();
 
         loadApiKey();
 
-        binding.acceptKey.setOnClickListener((view) ->
-                preferences.edit().putString("API_KEY", binding.apiKeyInput.getEditText().getText().toString()).apply()
+        binding.acceptKey.setOnClickListener((view) -> {
+                    preferences.edit().putString("API_KEY", binding.apiKeyInput.getEditText()
+                            .getText().toString()).apply();
+                    binding.dialog.setVisibility(View.GONE);
+                    binding.content.setVisibility(View.VISIBLE);
+                }
         );
     }
 
     private void loadApiKey() {
         if ((apiKey = preferences.getString("API_KEY", "")).equals("")) {
-            binding.dialogBackground.setVisibility(View.VISIBLE);
+            binding.dialog.setVisibility(View.VISIBLE);
+            binding.content.setVisibility(View.GONE);
+        } else {
+            processStats();
         }
+    }
+
+    private void processStats() {
+        api.getStats(apiKey, new APICallback() {
+            @Override
+            public void onSuccessful(WakatimeStats jsonObject) {
+                runOnUiThread(() -> {
+                    stats = jsonObject;
+                });
+            }
+
+            @Override
+            public void onFailure(IOException e) {
+                Log.e(WakatimeAPI.getTag(), "error", e);
+            }
+        });
     }
 }
